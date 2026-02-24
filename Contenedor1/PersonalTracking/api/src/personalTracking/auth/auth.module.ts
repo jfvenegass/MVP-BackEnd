0b6940b82dfb7ae05870abae919e9e58d2a63387
aaroncabc <aaroncabrales@uninorte.edu.co>
2026-02-23 22:10:37 -0500

    solucion bug de imports

--- Contenedor1/PersonalTracking/api/src/personalTracking/auth/auth.module.ts
+++ Contenedor1/PersonalTracking/api/src/personalTracking/auth/auth.module.ts
@@ -3,9 +3,10 @@ import { ConfigModule } from '@nestjs/config';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { ProfilesModule } from '../profiles/profiles.module';
+import { GameStatsModule } from '../game-stats/game-stats.module';
 
 @Module({
-  imports: [ConfigModule, ProfilesModule], // ProfilesModule si inyectas ProfilesService
+  imports: [ConfigModule, ProfilesModule, GameStatsModule], // ProfilesModule si inyectas ProfilesService
   providers: [AuthService],
   controllers: [AuthController],
 })
